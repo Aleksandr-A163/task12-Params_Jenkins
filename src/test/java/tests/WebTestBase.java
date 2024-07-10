8735eef06205369111a551cbac831bd4168f2033
Aleksandr Anosov <you@example.com>
2024-07-10 15:47:33 +0300

    updated WebTestBase

--- src/test/java/tests/WebTestBase.java
+++ src/test/java/tests/WebTestBase.java
@@ -1,4 +1,5 @@
 package tests;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -7,6 +8,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
 import java.util.Map;
 
 public class WebTestBase {
@@ -19,6 +21,7 @@ public class WebTestBase {
         Configuration.browserVersion = System.getProperty("brozerVersion", "121");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@" + System.getProperty("selenoidUrl", "selenoid.autotests.cloud") + "/wd/hub";
+        //Configuration.holdBrowserOpen = true;
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
